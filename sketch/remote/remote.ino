1f3a03bde034ac7916b8df69d1fc3e74573cef62
Ondrej Hejda <ondrejh.ck@email.cz>
2023-12-04 19:44:58 +0100

    basic control

--- sketch/remote/remote.ino
+++ sketch/remote/remote.ino
@@ -89,6 +89,20 @@ void handleStatic(int i) {
 
 void handleData() {
   digitalWrite(led, 0);
+  if (server.method() == HTTP_POST) {
+    StaticJsonDocument<256> rec;
+    DeserializationError error = deserializeJson(rec, server.arg("plain"));
+    if (error) {
+      Serial.println("Deserialization error.");
+    }
+    else {
+      doc["control"]["up"] = rec["btnUp"];
+      doc["control"]["down"] = rec["btnDown"];
+      doc["control"]["left"] = rec["btnLeft"];
+      doc["control"]["right"] = rec["btnRight"];
+      doc["control"]["light"] = rec["btnLight"];
+    }
+  }
   String output;
   serializeJson(doc, output);
   server.send(200, "application/json", output);
@@ -137,6 +151,7 @@ void setup(void) {
     server.send(200, "text/plain", "this works as well");
   });*/
   server.on("/data.json", handleData);
+  server.on("/data.json", HTTP_POST, handleData);
 
   server.on(fnames[0], [](){handleStatic(0);});
   server.on(fnames[1], [](){handleStatic(1);});
@@ -151,22 +166,4 @@ void setup(void) {
 void loop(void) {
   server.handleClient();
   delay(2);//allow the cpu to switch to other tasks
-}
-
-void drawGraph() {
-  String out = "";
-  char temp[100];
-  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-  out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x += 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
-
-  server.send(200, "image/svg+xml", out);
 }
