2ab371cb994ea5fd8436995bfad07f83d881afb6
Ondrej Hejda <ondrejh.ck@email.cz>
2023-12-06 17:03:48 +0100

    control outputs, new feature beep

--- sketch/remote/remote.ino
+++ sketch/remote/remote.ino
@@ -51,6 +51,29 @@
   ESP8266WebServer server(80);
 #endif
 
+const int PWM_CHANNEL_1 = 0;    // ESP32 has 16 channels which can generate 16 independent waveforms
+const int PWM_CHANNEL_2 = 1;
+const int PWM_CHANNEL_3 = 2;
+const int PWM_CHANNEL_4 = 3;
+const int PWM_FREQ = 500;     // Recall that Arduino Uno is ~490 Hz. Official ESP32 example uses 5,000Hz
+const int PWM_RESOLUTION = 8;
+const int PWM_OUT1 = 19;
+const int PWM_OUT2 = 18;
+const int PWM_OUT3 = 17;
+const int PWM_OUT4 = 16;
+
+const int SERVO_FREQ = 50;
+const int SERVO_RES = 12;
+const int SERVO_CH1 = 4;
+const int SERVO_CH2 = 5;
+const int SERVO_OUT1 = 13;
+const int SERVO_OUT2 = 14;
+
+const int OC_CH1 = 26;
+const int OC_CH2 = 27;
+
+const int BEEP = 32;
+
 StaticJsonDocument<512> doc;
 
 const int led = 2;
@@ -111,6 +134,7 @@ void handleData() {
       doc["control"]["left"] = rec["btnLeft"];
       doc["control"]["right"] = rec["btnRight"];
       doc["control"]["light"] = rec["btnLight"];
+      doc["control"]["beep"] = rec["btnBeep"];
     }
   }
   String output;
@@ -121,12 +145,50 @@ void handleData() {
 
 void setup(void) {
   pinMode(led, OUTPUT);
-  pinMode(bat, INPUT); // battery analog input
   digitalWrite(led, 0);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
+
+  // battery voltage adc input
+  pinMode(bat, INPUT); // battery analog input
+
+  // open ocllector outputs
+  pinMode(OC_CH1, OUTPUT);
+  pinMode(OC_CH2, OUTPUT);
+  digitalWrite(OC_CH1, 0);
+  digitalWrite(OC_CH2, 0);
+
+  // beeper output
+  pinMode(BEEP, OUTPUT);
+  digitalWrite(BEEP, 0);
+
+  // motor pwm
+  ledcSetup(PWM_CHANNEL_1, PWM_FREQ, PWM_RESOLUTION);
+  ledcSetup(PWM_CHANNEL_2, PWM_FREQ, PWM_RESOLUTION);
+  ledcSetup(PWM_CHANNEL_3, PWM_FREQ, PWM_RESOLUTION);
+  ledcSetup(PWM_CHANNEL_4, PWM_FREQ, PWM_RESOLUTION);
+
+  ledcAttachPin(PWM_OUT1, PWM_CHANNEL_1);
+  ledcAttachPin(PWM_OUT2, PWM_CHANNEL_2);
+  ledcAttachPin(PWM_OUT3, PWM_CHANNEL_3);
+  ledcAttachPin(PWM_OUT4, PWM_CHANNEL_4);
+
+  ledcWrite(PWM_CHANNEL_1, 0);
+  ledcWrite(PWM_CHANNEL_2, 0);
+  ledcWrite(PWM_CHANNEL_3, 0);
+  ledcWrite(PWM_CHANNEL_4, 0);
+
+  // servo pwm
+  ledcSetup(SERVO_CH1, SERVO_FREQ, SERVO_RES);
+  ledcSetup(SERVO_CH2, SERVO_FREQ, SERVO_RES);
+  
+  ledcAttachPin(SERVO_OUT1, SERVO_CH1);
+  ledcAttachPin(SERVO_OUT2, SERVO_CH2);
+
+  setServo(0.0, SERVO_CH1); //ledcWrite(SERVO_CH1, 614);
+  setServo(0.0, SERVO_CH2); //ledcWrite(SERVO_CH1, 614);
 
   JsonObject jsroot = doc.to<JsonObject>();
   jsroot["cnt"] = 0.0;
@@ -136,6 +198,7 @@ void setup(void) {
   jscontrol["up"] = false;
   jscontrol["down"] = false;
   jscontrol["light"] = false;
+  jscontrol["beep"] = false;
   JsonObject jsactual = jsroot.createNestedObject("actual");
   jsactual["acc"] = 0.0;
   jsactual["dir"] = 0.0;
@@ -189,12 +252,82 @@ bool pollBatteryVoltage(uint32_t now) {
   return false;
 }
 
+float addVal(bool add, bool sub, float val, float dt, float speed, float decay) {
+  float v = val;
+  if (add) {
+    v += dt * speed;
+    if (v > 1.0)
+      v = 1.0;
+  }
+  else if (sub) {
+    v -= dt * speed;
+    if (v < -1.0)
+      v = -1.0;
+  }
+  else if (v > 0.0) {
+    v -= dt * decay;
+    if (v < 0.0) {
+      v = 0.0;
+    }
+  }
+  else if (v < 0.0) {
+    v += dt * decay;
+    if (v > 0.0) {
+      v = 0.0;
+    }
+  }
+  return v;
+}
+
+void setPWM(float val, const int pwmP, const int pwmN) {
+  int dc = 255.0 * abs(val);
+  if (dc > 255)
+    dc = 255;
+  if (val > 0) {
+    ledcWrite(pwmN, 0);
+    ledcWrite(pwmP, dc);
+  }
+  else {
+    ledcWrite(pwmP, 0);
+    ledcWrite(pwmN, dc);
+  }
+}
+
+void setServo(float val, const int pwm) {
+  int dc = 204.8 * (1.5 + val / 2.0);
+  if (dc > 410) dc = 410;
+  if (dc < 205) dc = 205;
+  ledcWrite(pwm, dc);
+}
+
+bool pollControl(uint32_t now) {
+  static uint32_t tspd = 0;
+  if ((now - tspd) >= 5) {
+    float acc = addVal(doc["control"]["up"], doc["control"]["down"], doc["actual"]["acc"], 0.005, 3.0, 1.0);
+    float dir = addVal(doc["control"]["right"], doc["control"]["left"], doc["actual"]["dir"], 0.005, 3.0, 1.0);
+    setPWM(acc, PWM_CHANNEL_1, PWM_CHANNEL_2);
+    setPWM(acc, PWM_CHANNEL_3, PWM_CHANNEL_4);
+    setServo(dir, SERVO_CH1);
+    setServo(dir, SERVO_CH2);
+    doc["actual"]["dir"] = dir;
+    doc["actual"]["acc"] = acc;
+    bool light = doc["control"]["light"];
+    digitalWrite(OC_CH1, light);
+    digitalWrite(OC_CH2, light);
+    digitalWrite(BEEP, doc["control"]["beep"]);
+    tspd = now;
+    return true;
+  }
+  return false;
+}
+
 void loop(void) {
   server.handleClient();
   
   uint32_t now = millis();
   static uint32_t tcnt = 0;
-  if (pollBatteryVoltage(now)) {}
+  if (pollControl(now)) {}
+  else if (pollBatteryVoltage(now)) {}
   else if ((now - tcnt) >= 1000) {
     uint32_t cnt = doc["cnt"];
     cnt ++;
--- todo.md
+++ todo.md
@@ -1,5 +1,36 @@
+# ToDo:
+
 - [ ] AP mode
-- [ ] control output (acceleration and dirrection)
+- [x] control output (acceleration and dirrection)
 - [ ] turn output values to PWM (and servo)
 - [ ] HW prototyp, schema
 - [x] ADC měření napětí akumulátoru
+
+## HW:
+
+ADC měření napětí akumulátoru:
+
+- dělič 1k0, 4k7
+- pin ADC1_0, GPIO36
+- ADC2_x nelze použít když je WiFi (viz https://lastminuteengineers.com/esp32-basics-adc)
+- filtr 100nF kondenzátor
+
+PWM motory:
+
+- MX1508 driver (2 x full bridge)
+- GPIO16 -> IN4
+- GPIO17 -> IN3
+- GPIO18 -> IN2
+- GPIO19 -> IN1
+
+PWM serva:
+
+- GPIO13, GPIO14
+
+OC světla:
+
+- GPIO26, GPIO27
+
+Bzučák:
+
+- GPIO32
